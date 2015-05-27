0f498639f13776a8b4de2424f4ac09bf25e75205
Tony DeFreitas <anthony.defreitas@gmail.com>
2015-05-27 17:52:32 +0900

    that purple

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ window.onload = function() {
 };
 
 var randomPurple = function () {
-	var h = Math.random() * (310 - 290) + 290;
+	var h = Math.round(Math.random() * (310 - 290) + 290);
 	var s = Math.round(Math.random() * (80 - 20)) + 20;
 	var l = Math.round(Math.random() * (65 - 20)) + 20;
-	return ['hsl','(', h,',', s, '%,', l, '%'].join('');
+	return ['hsl','(', h,', ', s, '%, ', l, '%',")"].join('');
 };
 
 var calcNumSquares = function() {
@@ -20,7 +20,9 @@ var calcNumSquares = function() {
 var purpleSpan = function() {
 	var span = document.createElement('span');
 	span.classList.add('purple');
-	span.style.background = randomPurple();
+	var purple = randomPurple();
+	span.setAttribute('hsl', purple);
+	span.style.background = purple;
 	span.addEventListener('click', bigPurpleInfo);
 	container.appendChild(span);
 };
@@ -33,8 +35,11 @@ var renderPurpleSpans = function() {
 };
 
 var bigPurpleInfo = function() {
-	modal.style.background = this.style.background
+	modal.style.background = this.style.background;
 	modal.classList.add('visible');
+	hex.innerHTML = hexValues(this.style.background);
+	hsl.innerHTML = this.getAttribute('hsl');
+	rgb.innerHTML = this.style.background;
 	var close = modal.querySelector('div');
 	close.addEventListener('click', closeModal);
 	addFilter();
@@ -52,3 +57,17 @@ var addFilter = function() {
 var closeFilter = function() {
 	filter.classList.remove('haze');
 };
+
+var hexValues = function(rgbValues) {
+	var rgb = rgbValues.slice(4, rgbValues.length - 1).split(', ');
+	var r = rgb[0];
+	var g = rgb[1];
+	var b = rgb[2];
+	return ['hex #', numToHex(r), numToHex(g), numToHex(b)].join('');
+}
+
+var numToHex = function(num) {
+	var hexCode = parseInt(num).toString(16);
+	if (hexCode.length < 2) { hexCode = "0" + hexCode; }
+	return hexCode;
+}
